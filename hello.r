d9789065d09d64bff1ab58fbfe2c5c2ba72407e6
chenghjilai <chengjilai@sjtu.edu.cn>
2026-05-28 21:54:24 +0800

    feat: font

--- hello.r
+++ hello.r
@@ -1,1 +1,1 @@
-ggplot2::ggsave(filename="hello.svg", plot =ggplot2::ggplot() + ggplot2::geom_point(ggplot2::aes(x = c(1,2,3), y = c(2,4,6)))+ggplot2::labs(x = "x", y = "y"), width=200, height=200, units="mm")
+ggplot2::ggsave(filename="hello.svg", plot =ggplot2::ggplot() + ggplot2::geom_point(ggplot2::aes(x = c(1,2,3), y = c(2,4,6)))+ggplot2::labs(x = "x", y = "y") + ggplot2::theme(axis.title = ggplot2::element_text(family = "Maple Mono"), axis.text = ggplot2::element_text(family = "Maple Mono")), width=200, height=200, units="mm")
